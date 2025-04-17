eafa0d2f02b04da4b54b8ba88eceba2beea59be1
LopezMat <matthieulopezpro66@gmail.com>
2025-04-17 09:04:40 +0200

    Initial Commit

---
+++ Ghibli/GhibliApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliApp.swift
+//  Ghibli
+//
+//  Created by Stagiaire on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GhibliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
